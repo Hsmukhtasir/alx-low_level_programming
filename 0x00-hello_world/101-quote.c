50ce5fdfe08cc6099990da1c4419d71570d6f3e5
HsMukhtasir <hamza.fatale06@gmail.com>
2023-09-10 07:58:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19", 58);
-	write(1, "\n", 1);
+	fwrite(stderr, "and that piece of art is usefu\" - Dora Korpar, 2015-10-19", 58);
+	fwrite(stderr, "\n", 1);
 	return (1);
 }
