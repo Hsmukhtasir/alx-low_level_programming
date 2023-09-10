553c882c6c5539822320c29e0265511f0816ee96
HsMukhtasir <hamza.fatale06@gmail.com>
2023-09-10 07:50:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- *main - Write a C program that prints exactly "and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *main - Write a C program that prints exactly a sentence
  *
  * Return: (1)
  */
