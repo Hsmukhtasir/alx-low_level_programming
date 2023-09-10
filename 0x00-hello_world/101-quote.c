7a6da30b0931a7e01597a7ac735fec870b643f6b
HsMukhtasir <hamza.fatale06@gmail.com>
2023-09-10 07:37:43 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Write a C program that prints exactly "and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *
+ * Return: (1)
+ */
+int main(void)
+{
+	write(1,"and that piece of art is usefu\" - Dora Korpar, 2015-10-19",58);
+	return (1);
+}
