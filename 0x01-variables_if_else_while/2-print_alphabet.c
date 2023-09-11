3fdddd7f0b3fe55dfb41cd5eb9d21b8dab7a0732
HsMukhtasir <hamza.fatale06@gmail.com>
2023-09-11 14:10:30 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Printing Alphabet in Lower case
+ *
+ * Return: (0) Always Success!
+ */
+
+int main(void)
+{
+	char n;
+
+
+	for (n = 'a'; n <= 'z'; n++;)
+		putchar(n);
+		putchar("\n");
+	return (0);
