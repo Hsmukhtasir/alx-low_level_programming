47c8d3b143a32eada3961384fba06253375d3271
HsMukhtasir <hamza.fatale06@gmail.com>
2023-09-12 09:11:10 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Printing All Numbers of base 10
+ *
+ * Return: (0) Always Success!
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 30; n <= 39; n++)
+	{
+		putchar(n);
+	}
+		putchar("\n");
+	return (0);
+}
