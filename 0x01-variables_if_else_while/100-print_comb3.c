561811bf170962da52d5e9909d72465dbcebc35a
HsMukhtasir <hamza.fatale06@gmail.com>
2023-09-12 16:51:08 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Printing Numbers in a specific way
+ *
+ * Return: (0) Always Success!
+ */
+
+int main(void)
+{
+	int n, m, comma, space, l;
+
+	comma = (char) ',';
+	space = (char) ' ';
+	l = (char) '\n';
+	n = -1;
+
+	while (n <= 8)
+	{
+		n++;
+		while (m <= 9)
+		{
+			m = n + 1;
+			putchar(n);
+			putchar(m);
+			putchar(comma);
+			putchar(space);
+			m++;
+		}
+	}
+		return (0);
+}
