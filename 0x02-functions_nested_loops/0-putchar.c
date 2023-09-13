7b154dac3ddc957f15593dbd70c91b030bc240af
HsMukhtasir <hamza.fatale06@gmail.com>
2023-09-13 13:25:46 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Printing A Sentence
+ *
+ * Return: (0) Always Success!
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
