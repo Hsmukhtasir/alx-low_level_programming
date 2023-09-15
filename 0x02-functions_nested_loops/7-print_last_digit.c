e177faeb57ca686d1c47d33ad1560e2d942c34fc
HsMukhtasir <hamza.fatale06@gmail.com>
2023-09-15 09:08:39 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -16,12 +16,14 @@ int print_last_digit(int n)
 	l = '\n';
 	if (n >= 0 && n <= 9)
 	{
+		return (n);
 		write(1, &n, 1);
 	}
 	else
 	{
 		n = n % 10;
 		return (n);
+		write(1, &n, 1);
 	}
 	_putchar(l);
 }
