a0186f2f1f2defa969e17deeac07d068b3241b87
HsMukhtasir <hamza.fatale06@gmail.com>
2023-09-15 06:58:37 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @c: input
+ *
+ * Return: (0) 
+ */
+
+int _abs(int c)
+{
+	char l;
+	int n;
+
+	l = '\n';
+	n = (c * -1);
+	_putchar(n);
+	_putchar(l);
+	return (0);
+}
